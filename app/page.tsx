e58c8b3e6ff211b89d7c1e065647d6c2c53db381
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2023-07-04 18:47:28 +0530

    -> added < firebase.ts >

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
 "use client";
+import { db } from "@/firebase";
 import styles from "./page.module.css";
 import { Typography } from "@mui/material";
+import { onValue, ref } from "firebase/database";
+
+const getData = () => {
+	const starCountRef = ref(db);
+	let response;
+	onValue(starCountRef, (snapshot) => {
+		const data = snapshot.val();
+		console.log("data", data);
+		response = data;
+	});
+	return response;
+};
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <Typography align="center" variant="h1">
-        hello world
-      </Typography>
-    </main>
-  );
+	const data = getData();
+	return (
+		<main className={styles.main}>
+			<Typography align="center" variant="h1">
+				hello world
+				{`${data}`}
+			</Typography>
+		</main>
+	);
 }
